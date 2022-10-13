5165e2d8c36405f244bfa24c507e7e8c15ae092b
https://github.com/JavierGiberg <gibergjavier@gmail.com>
2022-10-13 17:19:17 +0300

    131022

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import LoginForm from "./components/LoginFrom";
+import FirebaseAuthService from "./FirebaseAuthService";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  FirebaseAuthService.subcribeToAuthChanse(setUser);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title-row">
+        <h1 className="title">Firebase Recipes</h1>
+        <LoginForm existinUser={user}></LoginForm>
+      </div>
     </div>
   );
 }
---
+++ src/components/LoginFrom.js
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import FirebaseAuthService from "../FirebaseAuthService";
+
+function LoginForm({ existinUser }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      await FirebaseAuthService.loginUser(username, password);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  function handleLogout() {
+    FirebaseAuthService.logoutUser();
+  }
+
+  async function handleSendResetPasswordEmail() {
+    if (!username) {
+      alert("Missing username");
+      return;
+    }
+    try {
+      await FirebaseAuthService.sendPasswordResetEmail(username);
+      alert("sent the password reset email");
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  async function handleLoginWithGoogle() {
+    try {
+      await FirebaseAuthService.loginWithGoogle();
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div className="login-form-container">
+      {existinUser ? (
+        <div className="row">
+          <h3>Welcome,{existinUser.email}</h3>
+          <button
+            type="button"
+            className="primary-button"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit} className="login-form">
+          <label className="input-label login-label">
+            Username (email);
+            <input
+              type="email"
+              required
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="input-text"
+            />
+          </label>
+          <label className="input-label login-label">
+            Password ;
+            <input
+              type="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input-text"
+            />
+          </label>
+          <div className="button-box">
+            <button className="primary-button">Login</button>
+            <button
+              type="button"
+              onClick={handleSendResetPasswordEmail}
+              className="primary-button"
+            >
+              Reset Password{" "}
+            </button>
+
+            <button
+              type="button"
+              className="primary-button"
+              onClick={handleLoginWithGoogle}
+            >
+              Login With Google
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+}
+
+export default LoginForm;
